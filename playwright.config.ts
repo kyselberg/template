ba49e1383a957e011f358f31892b37c000b4792a
illia kyselov <illia.kyselov@uni.tech>
2025-11-26 16:25:46 +0200

    create template

---
+++ playwright.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig, devices } from "@playwright/test";
+
+export default defineConfig({
+  testDir: "./tests",
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: "html",
+  use: {
+    baseURL: "http://localhost:5174",
+    trace: "on-first-retry",
+  },
+  projects: [
+    {
+      name: "Mobile Chrome",
+      use: { ...devices["iPhone 12"] },
+    },
+    {
+      name: "Mobile Safari",
+      use: { ...devices["iPhone 12"] },
+    },
+  ],
+  webServer: {
+    command: "npm run dev",
+    url: "http://localhost:5174",
+    reuseExistingServer: !process.env.CI,
+    timeout: 120 * 1000,
+  },
+});
