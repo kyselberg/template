dc150ec9da59e6ae796685202dd2f4cd23e61b4e
Illia Kyselov <doncerber@gmail.com>
2025-12-10 16:01:47 +0200

    Integrate vite-plugin-singlefile for optimized single-file output and update package dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "node:path";
 import { defineConfig } from "vite";
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
       "@": resolve(__dirname, "./src"),
